7db003f681f185fc98afb33e6130cb606db57875
Semen <semendubynin@gmail.com>
2020-06-01 12:18:13 +0300

    ColorMixLesson

--- ColorMix/ViewController.swift
+++ ColorMix/ViewController.swift
@@ -10,11 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mixView: UIView!
+    @IBOutlet weak var redSwitch: UISwitch!
+    @IBOutlet weak var greenSwitch: UISwitch!
+    @IBOutlet weak var blueSwith: UISwitch!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        mixView.backgroundColor = UIColor.blue
+        mixView.layer.cornerRadius = 100
+        mixView.layer.borderColor = UIColor.black.cgColor
+        mixView.layer.borderWidth = 3
     }
-
-
+    //MARK: IBOutlets
+    @IBAction func updateSlider(_ sender: UISlider) {
+        slidersUpdate()
+    }
+    @IBAction func redAction(_ sender: UISwitch) {
+        updateColor()
+    }
+    @IBAction func greenAction(_ sender: UISwitch) {
+        updateColor()
+    }
+    @IBAction func blueAction(_ sender: UISwitch) {
+        updateColor()
+    }
+    // MARK: Helpers
+    
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        mixView.backgroundColor = UIColor.clear
+        redSwitch.isOn = false
+        greenSwitch.isOn = false
+        blueSwith.isOn = false
+        redSlider.value = 0.0
+        greenSlider.value = 0.0
+        blueSlider.value = 0.0
+    }
+    func slidersUpdate() {
+        updateColor()
+        
+    }
+    
+    func updateColor() {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        if redSwitch.isOn {
+            red = CGFloat(redSlider.value)
+        }
+        if greenSwitch.isOn {
+            green = CGFloat(greenSlider.value)
+        }
+        if blueSwith.isOn {
+            blue = CGFloat(blueSlider.value)
+        }
+        let color = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        mixView.backgroundColor = color
+    }
 }
 
